c345e2a3544619d0a532465b8dd2cef478c70846
Maks Borenkov <borenkovmv@gmail.com>
2023-09-26 13:04:47 +0300

    sections

--- src/components/PackingList/PackingList.jsx
+++ src/components/PackingList/PackingList.jsx
@@ -1,12 +1,27 @@
 // import React, { useState } from "react";
 
+import { useState } from "react";
 import Items from "../Item/Item";
 
 const PackingList = ({ items, onToggleItem, onDeleteItem }) => {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items.slice((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Items
             item={item}
             key={item.id}
@@ -15,6 +30,18 @@ const PackingList = ({ items, onToggleItem, onDeleteItem }) => {
           />
         ))}
       </ul>
+      <div>
+        <select
+          className="actions"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="input">State to input</option>
+          <option value="description">State to description</option>
+          <option value="packed">State to packed </option>
+        </select>
+        <button className="actions">clear</button>
+      </div>
     </div>
   );
 };
--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Go pacingList🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numLength = items.filter((num) => num.packed).length;
+  const percentsTag = Math.round((numLength / numItems) * 100);
+
   return (
     <div className="stats">
       <footer>
-        <em>💼 You have 1 items on your list, and you already packed 1 (X%)</em>
+        <em>
+          {percentsTag === 100
+            ? "Go to planet ✈"
+            : `💼 You have ${numItems} items on your list, and you already packed
+          ${numLength}(${percentsTag}%)`}
+        </em>
       </footer>
     </div>
   );
